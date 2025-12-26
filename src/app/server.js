ebbaadf83d76e97bd76a99f605ffc40216754f7c
VishvaSywave <vishva.skywaveinfo@gmail.com>
2025-12-26 11:00:34 +0530

    error resolved

--- src/app/server.js
+++ src/app/server.js
@@ -1,13 +1,31 @@
-require("dotenv").config();
-const express = require("express");
-const connectDB = require("../lib/db");
+// require("dotenv").config();
+// const express = require("express");
+// const connectDB = require("../lib/db");
+
+// const app = express();
+
+// connectDB();
+// app.use(express.json());
+
+// app.use("/api/employees", require("./routes/employee.routes"));
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on port ${PORT}`);
+// });
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "../lib/db.js";
+import employeeRoutes from "./routes/employee.routes.js";
+
+dotenv.config();
 
 const app = express();
 
 connectDB();
 app.use(express.json());
 
-app.use("/api/employees", require("./routes/employee.routes"));
+app.use("/api/employees", employeeRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
