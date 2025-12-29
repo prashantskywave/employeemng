691260b2a044c4aeb7ec98b34010384dc417abb4
Dhurvi <dhruvi.skywaveinfo@gmail.com>
2025-12-29 19:12:36 +0530

    formateemployeeid

--- src/utils/formatEmployeeId.ts
+++ src/utils/formatEmployeeId.ts
@@ -1,4 +1,8 @@
 export function formatEmployeeId(id: string) {
   if (id.startsWith("EMP")) return id;
-  return `EMP${id.padStart(4, "0")}`;
+
+  const digitsOnly = id.replace(/\D/g, ""); // remove non-digits
+  const firstFour = digitsOnly.slice(0, 4);
+
+  return `EMP${firstFour.padStart(4, "0")}`;
 }
