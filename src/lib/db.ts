2958f772c7ff9264ba916aabf7caf802d2769ac1
froemailtest <128370484+froemailtest@users.noreply.github.com>
2025-12-31 09:34:39 +0530

    db.ts file changes

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,22 +3,14 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
-  throw new Error("Please define MONGODB_URI in .env.local");
-}
-
-let cached = (global as any).mongoose;
-
-if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null };
+  throw new Error("Please define MONGODB_URI in .env");
 }
 
 export async function connectDB() {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+  if (mongoose.connection.readyState === 1) {
+    return mongoose;
   }
 
-  cached.conn = await cached.promise;
-  return cached.conn;
+  await mongoose.connect(MONGODB_URI);
+  return mongoose;
 }
