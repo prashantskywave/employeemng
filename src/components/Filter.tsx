fb2481268b15d4341ff78f83abfe9e1c8460b4be
Dhurvi <dhruvi.skywaveinfo@gmail.com>
2026-01-06 17:35:28 +0530

    Cascading Selects

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -45,6 +45,7 @@ export default function Filters({
     "HR",
     "Accountant",
     "Project Manager",
+    "Team Lead",
   ];
 
   const availableRoles =
--- src/utils/departmentRoles.ts
+++ src/utils/departmentRoles.ts
@@ -2,5 +2,5 @@ export const departmentRoles: Record<string, string[]> = {
   Engineering: ["Frontend Developer", "Backend Developer"],
   Finance: ["Accountant"],
   HumanResources: ["HR"],
-  Manager: ["Project Manager", "HR"],
+  Manager: ["Project Manager", "HR", "Team Lead"],
 };
