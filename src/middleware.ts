d8aa963a506f7bcfd5bd6168d6a71bf4cdb2273d
DhruviSkywave <dhruvi.skywaveinfo@gmail.com>
2026-01-08 16:58:42 +0530

    Fix edge case on add and edit employee route

---
+++ src/middleware.ts
@@ -0,0 +1,43 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  const pathname = req.nextUrl.pathname;
+
+  if (pathname.startsWith("/employees/add")) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+
+    if (
+      token.department !== "admin"
+    ) {
+      return NextResponse.redirect(new URL("/employees", req.url));
+    }
+  }
+
+
+    if (pathname.startsWith("/employees/edit")) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+
+    if (
+      token.department !== "HumanResources" && token.department !== "Admin"
+    ) {
+      return NextResponse.redirect(new URL("/employees", req.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/employees/add"],
+};
